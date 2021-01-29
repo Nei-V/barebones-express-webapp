3aa21aa80255c984d580b7482532554dc059fd6f
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2021-01-29 19:45:31 +0000

    small correction

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(res)
   res.render('index', { title: 'Express' });
 });
 
 router.get('/:user_name', function(req, res, next) {
-  res.render('index', { title: req.params['name'] } );
+  console.log(req.params)
+  res.render('index', { title: req.params['user_name'] } );
+
 });
 
 module.exports = router;
